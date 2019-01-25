6b895f7162aed1eba92c5ef699f440e490cae36b
Abhishek Srivastava <abhishek.srivastava1@srmtechsol.com>
2019-01-25 16:20:16 +0530

    first commit

---
+++ precache-manifest.f70249df3d8513a076257fbd2d380bd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5b10e2def836aeb8109d",
+    "url": "/static/js/main.5b10e2de.chunk.js"
+  },
+  {
+    "revision": "f50debf22cceb166786e",
+    "url": "/static/js/1.f50debf2.chunk.js"
+  },
+  {
+    "revision": "5b10e2def836aeb8109d",
+    "url": "/static/css/main.9cdea60b.chunk.css"
+  },
+  {
+    "revision": "f50debf22cceb166786e",
+    "url": "/static/css/1.4ff941cd.chunk.css"
+  },
+  {
+    "revision": "762e7930dc6f04ea05443a2162fec247",
+    "url": "/index.html"
+  }
+];
